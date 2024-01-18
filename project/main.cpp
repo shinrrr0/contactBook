df1db507dabcd29d6f0e15a9ce0c16f4202c9969
shinrrr0 <roman.myshinsky3@gmail.com>
2024-01-18 21:20:14 +0500

    removed UserHandler

--- project/main.cpp
+++ project/main.cpp
@@ -4,38 +4,6 @@
 #include "commands.cpp"
 
 
-class UserHandler {
-private:
-	ContactBook* contact_book;
-	std::vector<Command*> commands{
-		new Help,
-		new ShowContacts{ contact_book },
-		new ChangeContact{ contact_book },
-		new DeleteContact{ contact_book }, 
-		new CreateContact{ contact_book },
-		new ClearContacts{ contact_book },
-		new SaveData{ contact_book },
-		new LoadData{ contact_book } };
-public:
-	void showStandardLabel() {
-		std::cout << "Введите команду (/help - список команд)" << std::endl;
-	}
-	void answerCommand(const std::string command) {
-		for (Command* elem : commands) {
-			if (elem->getTrigger() == command) {
-				(*elem)();
-			}
-		}
-	}
-	UserHandler(ContactBook* contact_book) : contact_book(contact_book) {}
-	~UserHandler() {
-		for (Command* elem : commands) {
-			delete elem;
-		}
-	}
-};
-
-
 int main() {
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
